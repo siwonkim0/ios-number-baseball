822e98b925b266a53fc07a4f57b090f1c90560bd
SIWONKIM <siyeon0616@gmail.com>
2021-10-06 10:43:39 +0900

    feat: 랜덤숫자배열 생성함수 구현

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,6 +6,17 @@ var chancesLeft = 9
 var userInputNumbers: [Int] = generateRandomNumbers()
 
 func generateRandomNumbers() -> [Int] {
-    return []
+    var result: [Int] = []
+
+    while result.count < 3 {
+        let randomNumber = Int.random(in: 0...9)
+        
+        if !result.contains(randomNumber) {
+            result.append(randomNumber)
+        }
+    }
+    
+    return result
 }
 
+print(generateRandomNumbers())
